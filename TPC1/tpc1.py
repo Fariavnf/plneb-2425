21f785d31aa6208b77a1113167edc4b9d7581930
Frederico Faria <49229552+Fariavnf@users.noreply.github.com>
2025-02-16 11:42:58 +0000

    Update tpc1.py
    added tests

--- TPC1/tpc1.py
+++ TPC1/tpc1.py
@@ -1,4 +1,4 @@
-# Python script for tpc1.
+# Python script do tpc1.
 
 def ex1(string):
     return string[::-1]
@@ -63,7 +63,7 @@ def ex8(string1, string2):
         while True:
             start = string2.find(string1, start)
             if start == -1:
-                break
+                return count
             count += 1
             start += 1
     else:
@@ -106,8 +106,39 @@ def ex10(stringList):
         return anagram_map
 
 
+def test(num_exercise, func, expected_output, *args):
+    try:
+        output = func(*args)
+        assert output == expected_output, f"Test {num_exercise} failed: Expected {expected_output}, but got {output}"
+        print(f"\n----- EX{num_exercise} PASSED -----")
+        print(f"Output: {output} -----> Expected: {expected_output} ")
+    except AssertionError as e:
+        print(f"\n----- EX{num_exercise} FAILED -----")
+        print(e)
 
-if __name__ == '__main__':
-    pass
-    #add tests later
+
+
+if __name__ == "__main__":
+
+    test(1, ex1, "aid mob", "bom dia")
+    test(2, ex2, {'a': 2, 'A': 1}, "boA tArde")
+    test(2, ex2, {'a': 0, 'A': 2}, "boA tArde")
+    test(3, ex3, 4, "bOa noIte")
+    test(3, ex3, 5, "bOa noIte")
+    test(4, ex4, "bom dia", "bOM diA")
+    test(5, ex5, "BOA TARDE", "Boa tardE")
+    test(6, ex6, True, "radar")
+    test(6, ex6, False, "boa noite")
+    test(7, ex7, True, "dois", "esternocleidomastoideu")
+    test(7, ex7, False, "esternocleidomastoideu", "bom dia")
+    test(8, ex8, 2, "aba", "abababa")
+    test(8, ex8, 3, "aba", "abababa")
+    test(8, ex8, 0, "abc", "")
+    test(9, ex9, True, "listen", "silent")
+    test(9, ex9, False, "hello", "world")
+    test(9, ex9, False, "roma", "")
+    test(9, ex9, True, "", "")
+    test(10, ex10,
+         {'amor': ['roma', 'amor'], 'aacss': ['sacas', 'casas'], 'egirt': ['tigre', 'trige'], 'aceimn': ['iceman', 'cinema'], 'hinoosz': ['sozinho']},
+         ["roma", "amor", "sacas", "casas", "tigre", "trige", "iceman", "cinema", "sozinho"])
 
